18b541d749cdbaa3599b3d98bac7439062d43e06
Anand <anand@anands-mac-pro.local>
2018-02-04 14:12:01 +0530

    Initial Commit

---
+++ Task-1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Task-1
+//
+//  Created by Anand on 04/02/18.
+//  Copyright © 2018 Rahul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
